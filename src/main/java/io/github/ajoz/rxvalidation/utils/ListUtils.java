6e5bafd2a49f4da90455063373828d403b9e161c
Andrzej Jóźwiak <andrzej.jozwiak@gmail.com>
2018-02-09 00:24:56 +0100

    License update

--- src/main/java/io/github/ajoz/rxvalidation/utils/ListUtils.java
+++ src/main/java/io/github/ajoz/rxvalidation/utils/ListUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2018-present, RxValidation contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.ajoz.rxvalidation.utils;
 
 import io.reactivex.annotations.NonNull;
@@ -13,7 +29,7 @@ public final class ListUtils {
     @NonNull
     public static <T> List<T> union(@NonNull final List<? extends T> first,
                                     @NonNull final List<? extends T> second) {
-        final List<T> result = new ArrayList<>(first);
+        final List<T> result = new ArrayList<T>(first);
         result.addAll(second);
         return Collections.unmodifiableList(result);
     }
